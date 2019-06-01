dcfa1b22b57f64bea5c5c2e86d5dd53f4c19d239
Lalo Martins <lalo.martins@gmail.com>
2019-06-01 13:18:20 +0300

    update post service for new protos

--- src/post_service.py
+++ src/post_service.py
@@ -51,15 +51,16 @@ class PostsServicer(post_pb2_grpc.PostsServicer):
       context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
       context.set_details('Post text can\'t be empty')
       raise Exception('Invalid arguments!')
-    if not (request.conversationTitle or request.inReplyTo):
+    if not (request.conversationId or request.inReplyTo):
       context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
-      context.set_details('Replies need inReplyTo, OPs need conversationTitle')
+      context.set_details('Replies need inReplyTo, OPs need conversationId')
       raise Exception('Invalid arguments!')
     post = post_pb2.Post()
     post.id = str(uuid.uuid4())
     post.text = request.text
     post.authorDisplayName = request.authorDisplayName or 'Anonymous Coward'
-    post.conversationTitle = request.conversationTitle
+    post.conversationId = request.conversationId
+    # TODO cache conversationTitle
     post.inReplyTo = request.inReplyTo
     post.created.GetCurrentTime()
     post.updated.FromNanoseconds(post.created.ToNanoseconds())
