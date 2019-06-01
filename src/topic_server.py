50ca8cf306c7e98eaf5b00f0f707ea750ea41d85
Lalo Martins <lalo.martins@gmail.com>
2019-06-01 21:03:28 +0300

    implement topic service

--- src/topic_server.py
+++ src/topic_server.py
@@ -4,5 +4,17 @@ from google.protobuf.empty_pb2 import Empty
 import topic_pb2
 import topic_pb2_grpc
 
+from db import topics, related
+
 class TopicsServicer(topic_pb2_grpc.TopicsServicer):
-  pass
+  def AllTopics(self, request, context):
+    return topic_pb2.TopicList(topics = topics)
+  
+  def Related(self, request, context):
+    res = set()
+    for (a, b) in related:
+      if a == request.name:
+        res.add(b)
+      elif b == request.name:
+        res.add(a)
+    return topic_pb2.TopicList(topics=list(res))
