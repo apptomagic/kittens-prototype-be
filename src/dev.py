b6ebe33baf032feeb17de49ded70b01a2029e290
Lalo Martins <lalo.martins@gmail.com>
2019-06-22 20:57:36 +0300

    try again to clean the CI log turns out the script does run in a tty on Travis… so remove that special casing

--- src/dev.py
+++ src/dev.py
@@ -4,11 +4,13 @@ def debug(*a, **kw):
   if is_verbose:
     print(*a, **kw)
 
-is_verbose = os.environ.get('DEBUG_VERBOSE')
-if isinstance(is_verbose, str):
-  verbose_req = is_verbose.lower()
+verbose_req = os.environ.get('DEBUG_VERBOSE')
+if isinstance(verbose_req, str) and verbose_req:
+  verbose_req = verbose_req.lower()
   if verbose_req in ('false', 'no', 'off'):
     is_verbose = False
-elif is_verbose is None and sys.stdout.isatty():
-  is_verbose = True
+  else:
+    is_verbose = True
+else:
+  is_verbose = False
 debug('Running with verbose debugging')
