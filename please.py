da29cbf3525061730f435fb40509baadd7b99613
Lalo Martins <lalo.martins@gmail.com>
2019-06-22 21:30:25 +0300

    automate generating protos for tests

--- please.py
+++ please.py
@@ -19,15 +19,44 @@ PROTOS = [
   'topic',
 ]
 
+
 def install_deps(config):
   # The pip docs specifically tell us not to do this… but IRL we can't trust our PATH in a cross-platform way, and we want to minimize upfront setup on  developer machines since this prototype is a temporary artifact
   from pip._internal import main as pip
   pip(['install', '-r', 'requirements.txt'])
 
+
 def install_test_deps(config):
   # The pip docs specifically tell us not to do this… but IRL we can't trust our PATH in a cross-platform way, and we want to minimize upfront setup on  developer machines since this prototype is a temporary artifact
   from pip._internal import main as pip
   pip(['install', '-r', 'kittens-api/bdd/behave/requirements.txt'])
+
+
+local_import_re = re.compile(r"import (.*_pb2) as (.*__pb2)")
+pkg_local_import_re = re.compile(r"from (.*) import (.*_pb2) as (.*__pb2)")
+def copy_and_fix_imports(filename, pkg = ''):
+  with open("src/{}".format(filename)) as f_in:
+    with open ("kittens-api/bdd/behave/features/client/{}".format(filename), "w") as f_out:
+      for line in f_in:
+        match = pkg_local_import_re.match(line)
+        if match:
+          i_pkg, i_mod, i_name = match.groups()
+          if i_pkg == pkg:
+            f_out.write("from . import {} as {}\n".format(i_mod, i_name))
+          elif i_pkg.startswith("google"):
+            f_out.write(line)
+          else:
+            f_out.write("from ..{} import {} as {}\n".format(i_pkg, i_mod, i_name))
+        else:
+          match = local_import_re.match(line)
+          if match:
+            i_mod, i_name = match.groups()
+            if pkg:
+              f_out.write("from .. import {} as {}\n".format(i_mod, i_name))
+            else:
+              f_out.write("from . import {} as {}\n".format(i_mod, i_name))
+          else:
+            f_out.write(line)
 
 def build_proto(config):
   from grpc_tools.protoc import main
@@ -47,6 +76,12 @@ def build_proto(config):
   res = main(args)
 
   if res: sys.exit(res)
+
+  # copy generated files to kittens-api but fix imports
+  for proto in PROTOS:
+    copy_and_fix_imports("{}_pb2.py".format(proto))
+    copy_and_fix_imports("{}_pb2_grpc.py".format(proto))
+
 
 def serve(config):
   subprocess.run(['python', './src/prototype_service.py'])
